1069589f7c287bd7fee9765228b0795ab7c75531
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-10-16 18:37:36 -0300

    fix: change to Prisma

--- src/models/Todo.ts
+++
@@ -1,7 +0,0 @@
-export default class Todo {
-  id!: string
-  title!: string
-  description!: string
-  createdAt!: Date | string
-  updatedAt!: Date | string
-}
--- src/repositories/database.ts
+++
@@ -1,5 +0,0 @@
-import { Database } from 'sqlite3'
-
-const db = new Database('./db/db.sqlite')
-
-export default db
