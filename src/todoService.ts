9ed26ee6a209644f73fa62ef0d58953873097eab
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-10-17 23:51:30 -0300

    fix: change updatedAt field when toggle

--- src/todoService.ts
+++ src/todoService.ts
@@ -151,6 +151,7 @@ export default function (prisma: PrismaClient): IServices {
         where: { id },
         data: {
           completed: !exists.completed,
+          updatedAt: new Date().toISOString(),
         },
       })
 
